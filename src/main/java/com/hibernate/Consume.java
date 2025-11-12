3b057cfa7e5cc294d5b62ce1461f3a18a2ca3aed
siddharthdmane <siddharthmane281441@gmail.com>
2025-11-12 23:57:17 +0530

    removed DTO implementation code from Consume.java to make it clean & code added in Consumer/Consumer.java

--- src/main/java/com/hibernate/Consume.java
+++ src/main/java/com/hibernate/Consume.java
@@ -1,7 +1,9 @@
 package com.hibernate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -22,16 +24,17 @@ public class Consume {
         Transaction txn = session.beginTransaction();
 
         // HQL Language
-        // List<Student> hqlStudents = session.createQuery("from Student",Student.class).list();
+        // List<Student> hqlStudents = session.createQuery("from
+        // Student",Student.class).list();
 
         // for(Student s : hqlStudents)
-        //     System.out.println(s);
+        // System.out.println(s);
 
         // Native/ Usual SQL Language
-        // List<Student> nativeStudents = session.createNativeQuery("select * from student",Student.class).list();
+        List<Student> nativeStudents = session.createNativeQuery("select * from student",Student.class).list();
 
-        // for(Student s : nativeStudents)
-        //     System.out.print(s);
+        for(Student s : nativeStudents)
+            System.out.print(s);
 
         // Parameterized Native Query
         // List <Object[]> obj = session.createNativeQuery("select s.address_id, s.name from student s where id = ?")
@@ -40,35 +43,7 @@ public class Consume {
 
         // System.out.println(obj.get(0)[0]+", "+obj.get(0)[1]);
 
-        // use of student-mobile dto
-        NativeQuery<Object[]> q = session.createNativeQuery("select s.id,s.name,s.gender,m.id,m.mobileNo from student s inner join mobile m on s.id = m.id");
-        List<Object[]> obj = q.getResultList();
-
-        List<studentMobile> sms = new ArrayList<>();
-
-        studentMobile temp = null;
-        List<Mobile> tempMobiles = new ArrayList<>();
-        int cnt;
-
-        for(Object[] ob : obj){
-            cnt = 0;
-            temp = new studentMobile();
-            temp.setsId((int)ob[cnt]); cnt++;
-            temp.setsName((String)ob[cnt]); cnt++;
-            temp.setsGender((char)ob[cnt]); cnt++;
-
-            Mobile tempMobile = new Mobile();
-            tempMobile.setId((int)ob[cnt]); cnt++;
-            tempMobile.setMobileNo((String)ob[cnt]);
-            
-            tempMobiles.add(tempMobile);
-
-            temp.setsMobiles(tempMobiles);
-        }
-
-        System.out.println(temp);
-
-        txn.commit();
+        // txn.commit();
         session.close();
         factory.close();
 
